1b25232c2b5db0d2eed7445fa8df76559931ad1a
Sansossio <juliosansossio@gmail.com>
2020-05-01 17:15:09 +0200

    Simple methods

--- src/main.py
+++ src/main.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from services.dolartoday import DolartodayService
+import json
 app = Flask(__name__)
 
+service = DolartodayService()
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/dollar')
+def dolar_price():
+    data = service.getDolarPrice()
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/eur')
+def eur_price():
+    data = service.getEuroPrice()
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 if __name__ == '__main__':
